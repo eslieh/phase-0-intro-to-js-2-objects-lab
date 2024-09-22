7a10d91a7142f6a0f38e720a26d98f2bd1620bf0
mashfied <victoreslieh@gmail.com>
2024-09-22 21:44:33 +0300

    passed test

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+let employee = {name:"Eslieh", streetAddress:"Buruburu Road"};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return {
+    ...employee,
+    [key]: value
+  };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key){
+    const new_employee = {...employee}
+    delete new_employee[key];
+    return new_employee;
+
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
